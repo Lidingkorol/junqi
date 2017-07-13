5b93419dab8a9170ee5875568fc900fa82628789
lidingkorol <799834285@qq.com>
2017-07-13 18:14:58 +0800

    t

---
+++ js/util.js
@@ -0,0 +1,22 @@
+
+
+
+var util = {};
+
+
+
+util.createPawnObj=function(obj) {
+	var o = new Object();
+	o.x = obj.x;
+	o.y = obj.y;
+	o.user = obj.user;
+	o.text = obj.text;
+	
+	return o;
+}
+
+
+
+util.findPath = function() {
+	
+}
