2266be8c812b7eb2196826cd229e36c4724f7236
lidingkorol <799834285@qq.com>
2017-07-17 19:16:28 +0800

    t

--- js/init.js
+++ js/init.js
@@ -2,11 +2,12 @@
 
 
 var chessboard = {
-	width : 320,
-	height : 320,
+	width : 960,
+	height : 960,
 	pawnList : [],
 	havePawn : [],
-	addStatus : []
+	addStatus : [],
+	layoutChess:[]
 };
 
 
@@ -16,9 +17,13 @@ chessboard.init = function () {
 	this.canvas.width = this.width;
 	this.canvas.height = this.height; 
 	this.dot();
-	this.setPawn();
+	this.layout();
+	this.clickEvent();
 	console.log(this)
 }
+
+
+
 
 
 
@@ -50,7 +55,6 @@ chessboard.dot = function () {
 		[-1,-1,-1,-1,-1,-1, 1, 1, 1, 1, 1,-1,-1,-1,-1,-1,-1 ]
 	];
 	
-	
 	for(var i=0;i<17;i++){
 		this.havePawn[i] = new Array();
 		for(var j=0;j<17;j++){
@@ -61,22 +65,75 @@ chessboard.dot = function () {
 }
 
 
-var testPawn = {x:1,y:2}
 
-chessboard.setPawn = function () {
-	for(var i=0;i<10;i++){
-		this.pawnList.push(util.createPawnObj(testPawn))
+chessboard.setPawn = function (chessList) {
+	console.log(chessList)
+	for(var i=0;i<chessList.length;i++) {
+		this.drawPawn(chessList[i])
 	}
 	
 }
 
 
-function findPath(start,end) {	
-	var allRoad = [];
-	for(var i=0;i<)
+chessboard.drawPawn = function(obj) {
+	var unit = this.width/16;
+	this.ct.beginPath();
+	this.ct.fillStyle = '#FF0000';
+	//this.ct.fillRect(obj.x*unit,obj.y*unit,obj.width,obj.height);
+	this.ct.rect(obj.x*unit-obj.width/2,obj.y*unit-obj.height/2,obj.width,obj.height);
+	this.ct.textAlign="center";
+    this.ct.font = "2pt Arial";
+    this.ct.strokeText(obj.text, obj.x*unit, obj.y*unit);
+    this.ct.strokeStyle = "red";
+    this.ct.stroke();
 }
 
 
+chessboard.layout = function() {
+	var qizi =[11,11,11,12,12,12,13,13,13,14,14,15,15,16,16,17,17,18,19,20,20,21,21,21,10];
+	var res = qizi;
+	var testArr = [];
+	for(var i=6;i<11;i++){
+		testArr[i] = new Array();
+		for(var j=11;j<17;j++){
+			if(!(((i==7||i==9)&&(j==14||j==12))||(i==8&&j==13))) {
+				var k = Math.floor(Math.random()*res.length)  
+				this.layoutChess.push(util.createPawnObj({x:i,y:j,value:res[k]}))
+				res.splice(k,1);
+			}
+		}
+	}
+	console.log(this.layoutChess)
+	this.setPawn(this.layoutChess);
+}
+
+
+chessboard.clickEvent = function() {
+	var that = this;
+	this.canvas.addEventListener('click',function(e) {
+		p = util.getEventPosition(e)
+		console.log(p)
+		console.log(that.ct.isPointInPath(p.x,p.y))
+	})
+}
+
+
+chessboard.reDraw = function(p) {
+	var that =this;
+	arr.forEach(function(v){
+		that.ct.clearRect(v.x*unit-v.width/2,v.y*unit-v.height/2,v.width,v.height);
+		that.ct.beginPath();
+		that.ct.textAlign="center";
+	    that.ct.font = "2pt Arial";
+	    that.ct.strokeText(v.text, v.x*unit, v.y*unit);
+	    that.ct.strokeStyle = "red";
+	    that.ct.stroke();
+	    if(p && ctx.isPointInPath(p.x, p.y)){
+	      	who.push(i);
+	    }
+		
+	})
+}
 
 //observer
 
@@ -112,6 +169,7 @@ observer.prototype.defineReactive = function(data,key,value){
 	observer(value);
 }
 */
+
 
 
 
--- js/util.js
+++ js/util.js
@@ -3,20 +3,82 @@
 
 var util = {};
 
-
+/*
+ * 工兵：11
+ * 排长：12
+ * 连长：13
+ * 营长：14
+ * 团长：15
+ * 旅长：16
+ * 师长：17
+ * 军长：18
+ * 司令：19
+ * 炸弹：20
+ * 地雷：21
+ * 军旗：10 
+ * 
+ */
 
 util.createPawnObj=function(obj) {
 	var o = new Object();
+	switch(obj.value) {
+		case 10:
+		o.text = '军旗';break;
+		case 11:
+		o.text = '工兵';break;
+		case 12:
+		o.text = '排长';break;
+		case 13:
+		o.text = '连长';break;
+		case 14:
+		o.text = '营长';break;
+		case 15:
+		o.text = '团长';break;
+		case 16:
+		o.text = '旅长';break;
+		case 17:
+		o.text = '师长';break;
+		case 18:
+		o.text = '军长';break;
+		case 19:
+		o.text = '司令';break;
+		case 20:
+		o.text = '炸弹';break;
+		case 21:
+		o.text = '地雷';break;
+	}
 	o.x = obj.x;
 	o.y = obj.y;
 	o.user = obj.user;
-	o.text = obj.text;
-	
+	o.value = obj.value;
+	o.width = 30;
+	o.height = 24;
 	return o;
 }
 
 
 
-util.findPath = function() {
-	
+util.shuffle = function(arr) {
+	if(typeof arr!='Array') {
+		return false;
+	}
+	var res = [];
+	for(var i=0;i<arr.length;i++) {
+		var j = Math.floor(Math.random()*arr.length) 
+		res[i] = arr[j];
+		arr.splice(j,1);
+	}
+	return res;
 }
+
+util.getEventPosition = function(ev){  
+  var x, y;  
+  if (ev.layerX || ev.layerX == 0) {  
+    x = ev.layerX;  
+    y = ev.layerY;  
+  } else if (ev.offsetX || ev.offsetX == 0) { // Opera  
+    x = ev.offsetX;  
+    y = ev.offsetY;  
+  }  
+  return {x: x, y: y};  
+}  
